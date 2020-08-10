eabaea53b1f16088f8c44d0132710d0ec5cdaa75
iamstmvasan <s.t.m.vasan@gmail.com>
2020-08-10 11:54:09 +0530

    Add new Student Data from Request Body using POST method

--- src/main/java/com/iamstmvasan/FirstProject/StudentController.java
+++ src/main/java/com/iamstmvasan/FirstProject/StudentController.java
@@ -2,9 +2,7 @@ package com.iamstmvasan.FirstProject;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,54 +11,12 @@ import java.util.Map;
 
 @RestController
 public class StudentController {
-    List<Student> studentList = new ArrayList<>();
+    private List<Student> studentList = new ArrayList<>();
 
-    @RequestMapping(value = "/allStudents")
-    public ResponseEntity allStudents(){
-        if(studentList.isEmpty()){
-            Map<String , String> map = new HashMap<>();
-            map.put("Message","Student data is empty, Create Data(student/create)");
-            ResponseEntity responseEntity = new ResponseEntity(map , HttpStatus.NOT_FOUND);
-            return responseEntity;
-        }
-        else{
-            ResponseEntity responseEntity = new ResponseEntity(studentList , HttpStatus.OK);
-            return responseEntity;
-        }
-    }
-   @RequestMapping(value = "/student/name/{name}")
-    public ResponseEntity fetchStudentByName(@PathVariable("name") String name){
-        for(Student student: studentList) {
-            if(student.getName().equalsIgnoreCase(name)) {
-                ResponseEntity responseEntity = new ResponseEntity(student, HttpStatus.OK);
-                return responseEntity;
-            }
-        }
-        Map<String, String> map = new HashMap<>();
-        map.put("message", "Student not found");
-        ResponseEntity responseEntity = new ResponseEntity(map, HttpStatus.NOT_FOUND);
-        return responseEntity;
-
-    }
-    @RequestMapping(value = "/student/id/{id}")
-    public ResponseEntity fetchStudentById(@PathVariable("id") int id){
-        for(Student student: studentList) {
-            if(student.getId() == id) {
-                ResponseEntity responseEntity = new ResponseEntity(student, HttpStatus.OK);
-                return responseEntity;
-            }
-        }
-        Map<String, String> map = new HashMap<>();
-        map.put("message", "Student not found");
-        ResponseEntity responseEntity = new ResponseEntity(map, HttpStatus.NOT_FOUND);
-        return responseEntity;
-
-    }
     //Initially i created two student's
     @RequestMapping(value = "student/create")
     public ResponseEntity initialStudentData(){
         ResponseEntity responseEntity;
-        if(studentList.isEmpty()) {
             Student student = new Student();
             student.setId(987);
             student.setName("Thiru");
@@ -81,8 +37,6 @@ public class StudentController {
             studentAddress.setPin(613204);
             student.setAddress(studentAddress);
 
-            studentList.add(student);
-
             Student student2 = new Student();
             student2.setId(969);
             student2.setName("Shahul");
@@ -100,42 +54,140 @@ public class StudentController {
             studentAddress2.setPin(613100);
             student2.setAddress(studentAddress);
 
-            studentList.add(student2);
+        boolean studentAdded = addStudentData(student);
+        boolean student2Added = addStudentData(student2);
+        if(studentAdded || student2Added){
             Map<String, String> map = new HashMap<>();
             map.put("Message", "Student data is successfully Created !");
             responseEntity = new ResponseEntity(map, HttpStatus.OK);
             return responseEntity;
         }
-
-        else {
+        else{
             Map<String, String> map = new HashMap<>();
-
             map.put("Message", "Student data is Already Created !");
             responseEntity = new ResponseEntity(map, HttpStatus.OK);
+            return responseEntity;
+        }
+    }
+
+    @RequestMapping(value = "/allStudents")
+    public ResponseEntity allStudents(){
+        ResponseEntity responseEntity;
+        if(studentList.isEmpty()){
+            Map<String , String> map = new HashMap<>();
+            map.put("Message","Student data is empty, Create Data(student/create)");
+            responseEntity = new ResponseEntity(map , HttpStatus.NOT_FOUND);
+            return responseEntity;
+        }
+        else{
+            responseEntity = new ResponseEntity(studentList , HttpStatus.OK);
+            return responseEntity;
+        }
+    }
+
+    @RequestMapping(value = "/student/name/{name}")
+    public ResponseEntity fetchStudentByName(@PathVariable("name") String name){
+        ResponseEntity responseEntity;
+        if(studentList.isEmpty()){
+            Map<String , String> map = new HashMap<>();
+            map.put("Message","Student data is empty, Create Data(student/create)");
+            responseEntity = new ResponseEntity(map , HttpStatus.NOT_FOUND);
+            return responseEntity;
+        }
+        else{
+            for(Student student: studentList) {
+                if(student.getName().equalsIgnoreCase(name)) {
+                    responseEntity = new ResponseEntity(student, HttpStatus.OK);
+                    return responseEntity;
+                }
+            }
+            Map<String, String> map = new HashMap<>();
+            map.put("message", "Student not found");
+            responseEntity = new ResponseEntity(map, HttpStatus.NOT_FOUND);
             return responseEntity;
         }
 
 
     }
-    @RequestMapping(value = "student/language/{language}")
-    public ResponseEntity fetchStudentByLanguage(@PathVariable("language") String language){
-        List<Student> list = new ArrayList<>();
-        boolean flag = false;
-        for(Student student : studentList)
-            if(student.getLanguage().contains(language)){
-                flag = true;
-                list.add(student);
-            }
-        ResponseEntity responseEntity;
-        if(flag)
-            responseEntity = new ResponseEntity(list , HttpStatus.OK);
-        else{
+
+    @RequestMapping(value = "/student/id/{id}")
+    public ResponseEntity fetchStudentById(@PathVariable("id") int id){
+        if(studentList.isEmpty()){
             Map<String , String> map = new HashMap<>();
-            map.put("Message" , "Students dont known "+language);
-            responseEntity = new ResponseEntity(map , HttpStatus.OK);
+            map.put("Message","Student data is empty, Create Data(student/create)");
+            ResponseEntity responseEntity = new ResponseEntity(map , HttpStatus.NOT_FOUND);
+            return responseEntity;
         }
-        return responseEntity;
+        else{
+            for(Student student: studentList) {
+                if(student.getId() == id) {
+                    ResponseEntity responseEntity = new ResponseEntity(student, HttpStatus.OK);
+                    return responseEntity;
+                }
+            }
+            Map<String, String> map = new HashMap<>();
+            map.put("message", "Student not found");
+            ResponseEntity responseEntity = new ResponseEntity(map, HttpStatus.NOT_FOUND);
+            return responseEntity;
+        }
     }
 
+    @RequestMapping(value = "/student/language/{language}")
+    public ResponseEntity fetchStudentByLanguage(@PathVariable("language") String language){
+        List<Student> list = new ArrayList<>();
+        boolean studentKnownLanguage = false;
+        if(studentList.isEmpty()){
+            Map<String , String> map = new HashMap<>();
+            map.put("Message","Student data is empty, Create Data(student/create)");
+            ResponseEntity responseEntity = new ResponseEntity(map , HttpStatus.NOT_FOUND);
+            return responseEntity;
+        }
+        else{
+            for(Student student : studentList)
+                if(student.getLanguage().contains(language)){
+                    studentKnownLanguage = true;
+                    list.add(student);
+                }
+            ResponseEntity responseEntity;
+            if(studentKnownLanguage)
+                responseEntity = new ResponseEntity(list , HttpStatus.OK);
+            else{
+                Map<String , String> map = new HashMap<>();
+                map.put("Message" , "Students dont known "+language);
+                responseEntity = new ResponseEntity(map , HttpStatus.OK);
+            }
+            return responseEntity;
+        }
 
+    }
+
+    @RequestMapping(value = "/addStudent" , method = RequestMethod.POST)
+    public ResponseEntity addStudent(@RequestBody Student addStudent){
+        ResponseEntity responseEntity;
+        Map<String , String> map = new HashMap<>();
+        if( addStudentData(addStudent) ){
+            map.put("Message" , "Student Added Successfully !");
+            responseEntity = new ResponseEntity(map , HttpStatus.OK);
+            return responseEntity;
+        }
+        else{
+            map.put("Message" , "Student Data is Already Added  !");
+            responseEntity = new ResponseEntity(map , HttpStatus.OK);
+            return responseEntity;
+        }
+
+    }
+
+    private boolean addStudentData(Student addStudent){
+        boolean studentAdded = true;
+        for(Student student : studentList)
+            if(student.getId() == addStudent.getId()){
+                studentAdded = false;
+                break;
+            }
+        if(studentAdded)
+            studentList.add(addStudent);
+        return studentAdded;
+
+    }
 }
